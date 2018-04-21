8c7edcac12b72c6eacbf18570dcf31ad0696883d
Robert Quan <rob.quan@gmail.com>
2018-04-21 16:27:03 -0600

    Editor working as challange asked - custom button creates dialog, dialog has first name, last name field. On submit, dialog closes and first name last name appear in editor

---
+++ public/scripts.js
@@ -0,0 +1,31 @@
+tinymce.init({
+    selector: '#tiny',
+    height: 500,
+    theme: 'modern',
+    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | mybutton',
+    plugins: 'lists advlist image imagetools',
+    setup: function(editor){
+        editor.addButton('mybutton', {
+        text: "Insert User Name",
+        onclick: function () {
+            editor.windowManager.open({
+            title: 'Insert User Name - Custom Dialog',
+            width: 720,
+            height: 110,
+            body: [
+              {type: 'textbox', name: 'firstName', label : 'First Name'},
+              {type: 'textbox', name: 'lastName', label : 'Last Name'}],
+            onSubmit: function(e){
+              console.log(e.data);
+              editor.setContent(`<span class="abc">${e.data.firstName}  ${e.data.lastName}</span>`)
+            }
+          })
+        }
+      })
+  }
+});
+
+
+// tinymce.WindowManager({
+//
+// })
